d9d651060b0ddd98a66f5252cf30ba5b5c983b82
Alina Kasianova <avkasyanova@gmail.com>
2021-10-12 20:26:52 +0300

    create component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import './App.css';
+import React, { Component } from 'react';
+// import Searchbar from './components/Searchbar';
+// import ImageGallery from './components/ImageGallery';
+// import ImageGalleryItem from './components/ImageGalleryItem';
+// import Button from './components/Button';
+// import Modal from './components/Modal';
+// import Loader from './components/Loader';
+import Container from './components/Container';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    query: 'rose',
+    page: 1,
+  };
+
+  async componentDidMount() {
+    const API_KEY = '23098575-38c072e060e8821b5779b85db';
+    // const BASE_URL = 'https://pixabay.com/api/'
+    fetch(
+      `https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+    ).then(response => response.json().then(query => this.setState(query)));
+  }
+  render() {
+    return (
+      <Container>
+        <div className="App">{this.state.query && <>тут будет разметка</>}</div>
+        ;
+      </Container>
+    );
+  }
 }
 
 export default App;
