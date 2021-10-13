147cf46de6867186652eca7485499754dfcc4d83
Alina Kasianova <avkasyanova@gmail.com>
2021-10-13 22:23:32 +0300

    component update

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -20,3 +20,19 @@
 // };
 
 // export default Button;
+
+import React from 'react';
+import s from './Button.module.css';
+import PropTypes from 'prop-types';
+
+export default function Button({ loadImages }) {
+  return (
+    <button type="button" className={s.btn} onClick={loadImages}>
+      Load more...
+    </button>
+  );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
