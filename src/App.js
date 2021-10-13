7c9f4bdbe2896a6a22514aac1b1499e70f64a6d9
Alina Kasianova <avkasyanova@gmail.com>
2021-10-13 15:18:37 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React, { Component } from 'react';
-// import Searchbar from './components/Searchbar';
-// import ImageGallery from './components/ImageGallery';
-// import ImageGalleryItem from './components/ImageGalleryItem';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Searchbar from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
+
 // import Button from './components/Button';
 // import Modal from './components/Modal';
 // import Loader from './components/Loader';
@@ -10,22 +12,23 @@ import Container from './components/Container';
 
 class App extends Component {
   state = {
-    query: 'rose',
-    page: 1,
+    searchQuery: '',
   };
 
-  async componentDidMount() {
-    const API_KEY = '23098575-38c072e060e8821b5779b85db';
-    // const BASE_URL = 'https://pixabay.com/api/'
-    fetch(
-      `https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-    ).then(response => response.json().then(query => this.setState(query)));
-  }
+  handleFormSubmit = searchQuery => {
+    this.setState({ searchQuery });
+    // console.log(searchQuery);
+  };
+
   render() {
+    const { searchQuery } = this.state;
     return (
       <Container>
-        <div className="App">{this.state.query && <>тут будет разметка</>}</div>
-        ;
+        <div className="App">
+          <Searchbar onSubmit={this.handleFormSubmit} />
+          <ImageGallery imageSearch={searchQuery} />
+          <ToastContainer autoClose={3000} />
+        </div>
       </Container>
     );
   }
