f451207e8e71cb6eba8ac83053fe4b657ee8e0ae
Alina Kasianova <avkasyanova@gmail.com>
2021-10-13 21:42:55 +0300

    fetch update

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './ImageGallery.module.css';
 import imagesAPI from '../../services/imagesApi';
-import ImageGalleryItem from '../ImageGalleryItem';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
 class ImageGallery extends Component {
   state = {
@@ -20,26 +20,41 @@ class ImageGallery extends Component {
 
     if (prevSearch !== currentSearch) {
       this.setState({ status: 'pending' });
+
       imagesAPI
         .fetchImages(currentSearch)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
+        .then(({ hits }) => {
+          if (hits.length === 0) {
+            this.setState({ status: 'rejected' });
+          } else {
+            console.log(hits);
+            this.setState(({ images, page }) => ({
+              images: [...images, ...hits],
+              page: page,
+              status: 'resolved',
+            }));
           }
-          return Promise.reject(
-            new Error(`No images for your request ${currentSearch}`),
-          );
         })
-        .then(({ hits }) =>
-          this.setState(({ images, page }) => ({
-            images: [...images, ...hits],
-            page: page,
-            status: 'resolved',
-          })),
-        )
         .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+    this.scrolling();
+  };
+
+  scrolling = () => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+    }, 1000);
+  };
+
   render() {
     const { images, error, status } = this.state;
 
@@ -57,25 +72,21 @@ class ImageGallery extends Component {
     if (status === 'resolved') {
       return (
         <ul className={s.list}>
-          {images.map(({ id, tags, webformatURL }) => {
-            return <ImageGalleryItem key={id} alt={tags} src={webformatURL} />;
+          {images.map(({ id, tags, webformatURL, largeImageURL }) => {
+            return (
+              <ImageGalleryItem
+                key={id}
+                id={id}
+                alt={tags}
+                src={webformatURL}
+                // dataLargeImg={largeImageURL}
+                // openModal={openModal}
+              />
+            );
           })}
         </ul>
       );
     }
-
-    //   <ul className={s.list}>
-    //     {images.map(({ id, tags, webformatURL, largeImageURL, onClick }) => {
-    //       return 'тут будет картинка';
-    //       // <ImageGalleryItem
-    //       //   key={id}
-    //       //   alt={tags}
-    //       //   src={webformatURL}
-    //       //   dataLargeImg={largeImageURL}
-    //       //   openModal={onClick}
-    //       // />
-    //     })}
-    //   </ul>
   }
 }
 
--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -1,11 +1,22 @@
 function fetchImages(searchQuery, page) {
   const API_KEY = '23098575-38c072e060e8821b5779b85db';
   const BASE_URL = 'https://pixabay.com/api/';
-  const url = `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
-  return fetch(url);
+  return fetch(
+    `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`,
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(
+      new Error(`No images for your request ${searchQuery}`),
+    );
+  });
 }
-const api = { fetchImages };
+const api = {
+  fetchImages,
+};
+
 export default api;
 
 // async componentDidMount() {
