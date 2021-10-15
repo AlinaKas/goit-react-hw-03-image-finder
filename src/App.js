b3f655494bba528ea9b19ef8a172dee3f46e8816
Alina Kasianova <avkasyanova@gmail.com>
2021-10-15 14:31:49 +0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import s from './App.module.css';
 import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Searchbar from './components/Searchbar';
-import ImageGallery from './components/ImageGallery';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+// import imagesAPI from './services/imagesApi';
 
-// import Button from './components/Button';
 // import Modal from './components/Modal';
-// import Loader from './components/Loader';
+// import Button from './components/Button';
+
+// import LoaderSpinner from './components/Loader';
 import Container from './components/Container';
 
 class App extends Component {
@@ -15,18 +17,64 @@ class App extends Component {
     searchQuery: '',
   };
 
+  // Сабмит формы и очистка
   handleFormSubmit = searchQuery => {
     this.setState({ searchQuery });
-    // console.log(searchQuery);
   };
 
+  // // Загрузка дальнейших картинок
+
+  // onLoadMore = () => {
+  //   this.setState(prevState => ({
+  //     page: prevState.page + 1,
+  //   }));
+  //   this.scrollPage();
+  // };
+
+  // //Скролл при подгрузке картинок
+  // scrollPage = () => {
+  //   setTimeout(() => {
+  //     window.scrollTo({
+  //       top: document.documentElement.scrollHeight,
+  //       behavior: 'smooth',
+  //     });
+  //   }, 1000);
+  // };
+
+  // //Модалка
+  // toggleModal = () => {
+  //   this.setState(({ showModal }) => ({ showModal: !showModal }));
+  // };
+
+  // openModal = e => {
+  //   this.setState(() => ({
+  //     modalImage: e.target.dataset.modal,
+  //     alt: e.target.alt,
+  //   }));
+  //   this.toggleModal();
+  // };
+
+  // РЕНДЕР СТРАНИЦЫ
+
   render() {
-    const { searchQuery } = this.state;
     return (
       <Container>
-        <div className="App">
+        <div className={s.App}>
           <Searchbar onSubmit={this.handleFormSubmit} />
-          <ImageGallery imageSearch={searchQuery} />
+          {/* {images.length > 0 && !error && ( */}
+          <>
+            <ImageGallery imageSearch={this.state.searchQuery} />
+            {/* <Button loadImages={this.onLoadMore} /> */}
+          </>
+          {/* )} */}
+          {/* {showModal && (
+            <Modal
+              onClose={this.toggleModal}
+              src={modalImage}
+              alt={alt}
+            ></Modal>
+          )}
+          {error && <p className={s.error}>{error}</p>} */}
           <ToastContainer autoClose={3000} />
         </div>
       </Container>
