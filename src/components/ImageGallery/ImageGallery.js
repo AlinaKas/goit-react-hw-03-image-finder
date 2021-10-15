f9efb1bcc128e514b03775f62f3103276ac9a3c1
Alina Kasianova <avkasyanova@gmail.com>
2021-10-15 18:53:25 +0300

    gallery update

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 // import imagesAPI from '../../services/imagesApi';
-
 // import Modal from '../Modal';
 // import Button from '../Button';
-
 // import LoaderSpinner from '../Loader';
+
+export default function ImageGallery({ images, onClick }) {
+  return (
+    <ul className={s.list}>
+      {images.map(({ id, tags, webformatURL, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          alt={tags}
+          src={webformatURL}
+          srcBigImg={largeImageURL}
+          onClick={onClick}
+        />
+      ))}
+    </ul>
+  );
+}
+
+ImageGallery.prototype = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+  onClick: PropTypes.func.isRequired,
+};
 
 // export default class ImageGallery extends Component {
 //   state = {
@@ -49,11 +72,6 @@ import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 //         .catch(error => this.setState({ error, status: 'rejected' }));
 //     }
 //   }
-
-//   // // Сабмит формы и очистка
-//   // handleFormSubmit = searchQuery => {
-//   //   this.setState({ searchQuery, images: [], page: 1, error: null });
-//   // };
 
 //   // Загрузка дальнейших картинок
 
@@ -129,144 +147,3 @@ import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 //     }
 //   }
 // }
-
-// ImageGallery.prototype = {
-//   images: PropTypes.arrayOf(PropTypes.shape).isRequired,
-//   onClick: PropTypes.func,
-// };
-
-export default function ImageGallery({ images, onClick }) {
-  return (
-    <ul className={s.list}>
-      {images.map(({ id, tags, webformatURL, largeImageURL }) => {
-        return (
-          <>
-            <ImageGalleryItem
-              key={id}
-              alt={tags}
-              src={webformatURL}
-              srcBigImg={largeImageURL}
-              onClick={onClick}
-            />
-          </>
-        );
-      })}
-    </ul>
-  );
-}
-
-// ImageGallery.prototype = {
-//   images: PropTypes.arrayOf(PropTypes.shape).isRequired,
-//   onClick: PropTypes.func,
-// };
-
-// export default ImageGallery;
-
-// import imagesAPI from '../../services/imagesApi';
-// import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-
-// import Loader from 'react-loader-spinner';
-// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-
-// class ImageGallery extends Component {
-//   state = {
-//     images: [],
-
-//     status: 'idle',
-//     page: 1,
-//     showModal: false,
-//     error: null,
-//   };
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const prevSearch = prevProps.imageSearch;
-//     const nextSearch = this.props.imageSearch;
-//     const prevPage = prevState.page;
-//     const nextPage = this.state.page;
-
-//     if (prevSearch !== nextSearch || prevPage !== nextPage) {
-//       this.setState({ status: 'pending' });
-
-//       imagesAPI
-//         .fetchImages(nextSearch, nextPage)
-//         .then(({ hits }) => {
-//           console.log({ hits });
-//           if (hits.length === 0) {
-//             this.setState({ status: 'rejected' });
-//           } else {
-//             // console.log(hits);
-//             this.setState(({ images, page }) => ({
-//               images: [...images, ...hits],
-//               page: page,
-//               status: 'resolved',
-//               //   searchQuery: nextSearch,
-//             }));
-//           }
-//         })
-//         .catch(error => this.setState({ error, status: 'rejected' }));
-//     }
-//   }
-
-//   onLoadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//     this.scrolling();
-//   };
-
-//   scrolling = () => {
-//     setTimeout(() => {
-//       window.scrollTo({
-//         top: document.documentElement.scrollHeight,
-//         behavior: 'smooth',
-//       });
-//     }, 1000);
-//   };
-
-//   render() {
-//     const { images, error, status } = this.state;
-
-//     if (status === 'idle') {
-//       return '';
-//     }
-//     if (status === 'pending') {
-//       return (
-//         <Loader
-//           className="s.loader"
-//           type="Oval"
-//           color="#00BFFF"
-//           height={120}
-//           width={120}
-//         />
-//       );
-//     }
-
-//     if (status === 'rejected') {
-//       return <p>{error.message}</p>;
-//     }
-
-//     if (status === 'resolved') {
-//       return (
-//         <>
-//           <ul className={s.list}>
-//             {images.map(({ id, tags, webformatURL, largeImageURL }) => {
-//               return (
-//                 <ImageGalleryItem
-//                   key={id}
-//                   id={id}
-//                   alt={tags}
-//                   src={webformatURL}
-//                   // dataLargeImg={largeImageURL}
-//                   // openModal={openModal}
-//                 />
-//               );
-//             })}
-//           </ul>
-//           <Button loadImages={this.onLoadMore} />
-//         </>
-//       );
-//     }
-//   }
-// }
-
-// export default ImageGallery;
