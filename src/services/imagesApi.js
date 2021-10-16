3e6f7a13f2e15a25925804d98277eebaae64ce03
Alina Kasianova <avkasyanova@gmail.com>
2021-10-16 11:43:50 +0300

    fetch update

--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -18,20 +18,3 @@ const api = {
 };
 
 export default api;
-
-// function fetchImages(searchQuery, page) {
-//   // axios.defaults.baseURL = BASE_URL;
-//   // axios.defaults.headers.common.Authorization = API_KEY;
-//   const API_KEY = '23098575-38c072e060e8821b5779b85db';
-//   const BASE_URL = 'https://pixabay.com/api/';
-//   const url = `${BASE_URL}?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-
-//   return axios.get(url).then(res => {
-//     return res.data;
-//   });
-// }
-// const api = {
-//   fetchImages,
-// };
-
-// export default api;
